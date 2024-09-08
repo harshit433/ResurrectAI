977ab1c91a811d571248a5d3d36998132040cba2
Harshit Aggarwal <67017828+harshit433@users.noreply.github.com>
2024-09-08 11:34:42 +0530

    Refactor chatbot functionality and dependencies
    This commit refactors the chatbot functionality in the app.py file. It removes the unused chatbot.py file and updates the code to use the unsloth library for chat templates and the FastLanguageModel for language processing. The max sequence length is set to 2048, and the model and tokenizer are initialized with the "ressurectAI/base" model. The tokenizer is also updated to use a chat template with specific mappings for role, content, user, and assistant. The chatbot response is generated using the model and tokenizer, and the result is returned to the user.

--- app.py
+++ app.py
@@ -1,7 +1,21 @@
-from chatbot import CB
 from flask import Flask, render_template, request
+from unsloth.chat_templates import get_chat_template
+from unsloth import FastLanguageModel
 
 app = Flask(__name__)
+
+max_seq_length = 2048
+model, tokenizer = FastLanguageModel.from_pretrained(
+    model_name="ressurectAI/base",
+    max_seq_length=max_seq_length,
+    dtype=None,
+)
+tokenizer = get_chat_template(
+    tokenizer,
+    mapping={"role": "from", "content": "value", "user": "Person", "assistant": "Gandhi"},
+    chat_template="chatml",
+)
+FastLanguageModel.for_inference(model)
 
 @app.route("/chatbot")
 def home():
@@ -10,5 +24,16 @@ def home():
 @app.route("/get")
 def get_bot_response():
     userText = request.args.get('msg')
-    return str(CB.get_response(userText))
+    messages = [
+    {"from": "Person", "value": "Gandhi ji, {}".format(userText)},
+    ]
+    inputs = tokenizer.apply_chat_template(
+    messages,
+    tokenize=True,
+    add_generation_prompt=False,
+    return_tensors="pt",
+    ).to("cuda")
+    a = model.generate(input_ids=inputs, max_new_tokens=1024,use_cache=True)
+    return str(tokenizer.batch_decode(a)[0].split('assistant\n')[1].replace('<|im_end|>','').strip())
+
 app.run(debug = True)
