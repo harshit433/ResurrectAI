23fc6406c23d9ecc8c972bf90348bd03687e7c98
Harshit Aggarwal <67017828+harshit433@users.noreply.github.com>
2024-09-11 12:02:32 +0530

    commit

--- app.py
+++ app.py
@@ -1,21 +1,21 @@
 from flask import Flask, render_template, request
-from unsloth.chat_templates import get_chat_template
-from unsloth import FastLanguageModel
+# from unsloth.chat_templates import get_chat_template
+# from unsloth import FastLanguageModel
 
 app = Flask(__name__)
 
-max_seq_length = 2048
-model, tokenizer = FastLanguageModel.from_pretrained(
-    model_name="ressurectAI/base",
-    max_seq_length=max_seq_length,
-    dtype=None,
-)
-tokenizer = get_chat_template(
-    tokenizer,
-    mapping={"role": "from", "content": "value", "user": "Person", "assistant": "Gandhi"},
-    chat_template="chatml",
-)
-FastLanguageModel.for_inference(model)
+# max_seq_length = 2048
+# model, tokenizer = FastLanguageModel.from_pretrained(
+#     model_name="ressurectAI/base",
+#     max_seq_length=max_seq_length,
+#     dtype=None,
+# )
+# tokenizer = get_chat_template(
+#     tokenizer,
+#     mapping={"role": "from", "content": "value", "user": "Person", "assistant": "Gandhi"},
+#     chat_template="chatml",
+# )
+# FastLanguageModel.for_inference(model)
 
 @app.route("/chatbot")
 def home():
@@ -27,13 +27,13 @@ def get_bot_response():
     messages = [
     {"from": "Person", "value": "Gandhi ji, {}".format(userText)},
     ]
-    inputs = tokenizer.apply_chat_template(
-    messages,
-    tokenize=True,
-    add_generation_prompt=False,
-    return_tensors="pt",
-    ).to("cuda")
-    a = model.generate(input_ids=inputs, max_new_tokens=1024,use_cache=True)
-    return str(tokenizer.batch_decode(a)[0].split('assistant\n')[1].replace('<|im_end|>','').strip())
+    # inputs = tokenizer.apply_chat_template(
+    # messages,
+    # tokenize=True,
+    # add_generation_prompt=False,
+    # return_tensors="pt",
+    # ).to("cuda")
+    # a = model.generate(input_ids=inputs, max_new_tokens=1024,use_cache=True)
+    # return str(tokenizer.batch_decode(a)[0].split('assistant\n')[1].replace('<|im_end|>','').strip())
 
 app.run(debug = True)
