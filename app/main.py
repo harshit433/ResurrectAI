06ed821a30eed63cfb3c3b4f7b8c53873d788731
Harshit Aggarwal <67017828+harshit433@users.noreply.github.com>
2024-10-14 12:15:43 +0530

    Refactor Dockerfile to improve package installation and remove unnecessary dependency

--- app/main.py
+++ app/main.py
@@ -1,13 +1,9 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
-import uvicorn
 from peft import AutoPeftModelForCausalLM
 from transformers import AutoTokenizer
 
 app = FastAPI()
-
-# Load the model from Hugging Face
-model_name = "ressurectAI/Gandhi10.0"
 
 model = AutoPeftModelForCausalLM.from_pretrained(
     "ressurectAI/Gandhi10.0", # YOUR MODEL YOU USED FOR TRAINING
@@ -20,7 +16,7 @@ class PredictionRequest(BaseModel):
     text: str
 
 @app.get("/")
-def welcome():
+def read_root():
     return {"message":"Welcome to the ResurrectAI"}
 
 @app.post("/predict")
@@ -40,6 +36,3 @@ async def predict(request: PredictionRequest):
         return {'Gandhi Ji':response}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
-
-if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000)
